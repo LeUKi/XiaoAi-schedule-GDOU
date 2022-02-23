25c9c2644813d38a805e8c18cf94ab9435f55354
Lafish <34953046+LeUKi@users.noreply.github.com>
2022-02-23 14:28:30 +0800

    Update scheduleHtmlParser.js

--- scheduleHtmlParser.js
+++ scheduleHtmlParser.js
@@ -5,6 +5,48 @@ function getSections(str, History) {
     let start = parseInt(str.split('-')[0])
     let end = parseInt(str.split('-')[1])
     let sections = []
+    let sectionTimes = [
+    {
+    section:1+ 0,
+    startTime: "08:10",
+    endTime: "08:55"
+}, {
+    section:1+ 1,
+    startTime: "09:00",
+    endTime: "09:45"
+}, {
+    section:1+ 2,
+    startTime: "10:15",
+    endTime: "11:00"
+}, {
+    section:1+ 3,
+    startTime: "11:05",
+    endTime: "11:50"
+}, {
+    section:1+ 4,
+    startTime: "14:30",
+    endTime: "15:15"
+}, {
+    section:1+ 5,
+    startTime: "15:20",
+    endTime: "16:05"
+}, {
+    section:1+ 6,
+    startTime: "16:30",
+    endTime: "17:15"
+}, {
+    section:1+ 7,
+    startTime: "17:20",
+    endTime: "18:05"
+}, {
+    section:1+ 8,
+    startTime: "19:30",
+    endTime: "20:15"
+}, {
+    section:1+ 9,
+    startTime: "20:25",
+    endTime: "21:10"
+}]
     for (let i = start; i <= end; i++) {
         sections.push({
 
@@ -107,55 +149,16 @@ function parseList(html) {
     return result
 }
 
-let sectionTimes = [{
-    "section": 0,
-    "startTime": "08:10",
-    "endTime": "08:55"
-}, {
-    "section": 1,
-    "startTime": "09:00",
-    "endTime": "09:45"
-}, {
-    "section": 2,
-    "startTime": "10:15",
-    "endTime": "11:00"
-}, {
-    "section": 3,
-    "startTime": "11:05",
-    "endTime": "11:50"
-}, {
-    "section": 4,
-    "startTime": "14:30",
-    "endTime": "15:15"
-}, {
-    "section": 5,
-    "startTime": "15:20",
-    "endTime": "16:05"
-}, {
-    "section": 6,
-    "startTime": "16:30",
-    "endTime": "17:15"
-}, {
-    "section": 7,
-    "startTime": "17:20",
-    "endTime": "18:05"
-}, {
-    "section": 8,
-    "startTime": "19:30",
-    "endTime": "20:15"
-}, {
-    "section": 9,
-    "startTime": "20:25",
-    "endTime": "21:10"
-}, ]
+
 
 function scheduleHtmlParser(html) {
     let result = []
     result = parseList(html)
 
     console.log(result.length)
-    return {
-        "courseInfos": result,
-        "sectionTimes": sectionTimes
-    }
+//     return {
+//         "courseInfos": result,
+//         "sectionTimes": sectionTimes
+//     }
+return result
 }
